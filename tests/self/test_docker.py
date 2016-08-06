66d2ede34815391d48732b1e5854bf496a78c550
Jiri Danek <jdanek@redhat.com>
2016-08-06 23:23:28 +0200

    Try few more things with docker
    Pull first
    Pull in .travis.yml so that tests run faster

--- tests/self/test_docker.py
+++ tests/self/test_docker.py
@@ -1,7 +1,20 @@
 import pytest
 
 import docker
+import docker.utils
+import subprocess
+import socket
 import time
+import traceback
+import contextlib
+
+
+ROUTER_IMAGE = 'jdanekrh/dispatch-router:4-90415a'
+SUBNET = '172.16.43.0/24'
+RA = '172.16.43.2'
+
+import docker
+import subprocess
 
 
 @pytest.fixture
@@ -14,16 +27,49 @@ def client(request):
 
 @pytest.fixture
 def network(request, client: docker.Client):
-    n = "mynetwork123"
-    client.create_network(n)
-    request.addfinalizer(lambda: client.remove_network(n))
-    return n
+    name = "dispatch"
+    ipam_pool = docker.utils.create_ipam_pool(subnet=SUBNET)
+    ipam_config = docker.utils.create_ipam_config(pool_configs=[ipam_pool])
+    network = client.create_network(name, driver='bridge', ipam=ipam_config)
+    net_id = network['Id']
+    request.addfinalizer(lambda: client.remove_network(net_id))
+    return net_id
 
-
-class DispatchRouter(object):
-    def __init__(self):
-        pass
 
 class TestDocker(object):
     def test_fixtures(self, network: str):
-        time.sleep(10)
+        stdout = subprocess.run("docker network ls",
+                                stdout=subprocess.PIPE,
+                                universal_newlines=True, shell=True, check=True).stdout
+        print(network)
+        assert network[:6] in stdout
+
+    @contextlib.contextmanager
+    def container(self, client: docker.Client, network: str):
+        container = None
+        try:
+            container = client.create_container(ROUTER_IMAGE, name="ra")
+            client.connect_container_to_network(container, net_id=network, ipv4_address=RA)
+            client.start(container)
+            yield container
+            client.kill(container)
+        finally:
+            if container is not None:
+                client.remove_container(container)
+
+    def test_run_container(self, client, network):
+        with self.container(client, network) as container:
+            subprocess.run(['ping', '-c1', RA], check=True)
+
+    def test_run_qdstat(self, client: docker.Client, network):
+        with self.container(client, network) as container:
+            while True:
+                try:
+                    with socket.create_connection((RA, 5672)):
+                        break
+                except ConnectionRefusedError:
+                    time.sleep(0.03)
+
+            exec = client.exec_create(container, "qdstat -g")
+            output = client.exec_start(exec).decode()
+            print(output)
